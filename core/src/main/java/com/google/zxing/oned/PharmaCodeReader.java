ceef5fc1f80b4569470ffad185a3d5d12c0cf3c6
Ruslan Karachun <free.changer@gmail.com>
2018-03-23 16:54:20 +0300

    Added debug info

--- core/src/main/java/com/google/zxing/oned/PharmaCodeReader.java
+++ core/src/main/java/com/google/zxing/oned/PharmaCodeReader.java
@@ -87,7 +87,7 @@ public final class PharmaCodeReader extends OneDReader {
     // Read off white space
     int nextStart = row.getNextSet(start[1]);
     int end = row.getSize();
-
+    System.out.print("["+ end +"]");
     int[] theCounters = counters;
     Arrays.fill(theCounters, 0);
     StringBuilder result = decodeRowResult;
