2a4420d26866f5742aaa5e25de372587c97fedba
Ruslan Karachun <free.changer@gmail.com>
2018-04-25 15:07:05 +0300

    Added counter

---
+++ core/src/main/java/com/google/zxing/oned/CodeElement.java
@@ -0,0 +1,21 @@
+package net.aptinfo;
+
+public class CodeElement {
+    private int iCode;
+    private long lUnixTime;
+
+    public String toString() {
+        return "code=" + Integer.toString(iCode)
+                + "|time=" + Long.toString(lUnixTime);
+    }
+    public CodeElement(int c, long t) {
+        iCode = c;
+        lUnixTime = t;
+    }
+    public int getCode() {
+        return iCode;
+    }
+    public long getTime() {
+        return lUnixTime;
+    }
+}
---
+++ core/src/main/java/com/google/zxing/oned/Counter.java
@@ -0,0 +1,60 @@
+package net.aptinfo;
+
+import com.sun.org.apache.bcel.internal.classfile.Code;
+import net.aptinfo.CircularList;
+import net.aptinfo.CodeElement;
+
+public class Counter {
+    private static Counter INSTANCE = null;
+    private int SIZE = 10;
+    private int counter;
+    private CodeElement[] cListArray;
+
+    private Counter() {
+        counter = 0;
+        cListArray = new CodeElement[SIZE];
+    }
+    public static synchronized Counter getInstance() {
+      if (INSTANCE == null) {
+        INSTANCE = new Counter();
+      }
+      return(INSTANCE);
+    }
+    // other instance methods can follow
+    public void addCode(int iCode) {
+        int i = counter % SIZE;
+        long unixTime = System.currentTimeMillis() / 1000L;
+        cListArray[i] = new CodeElement(iCode, unixTime);
+        counter ++;
+    }
+    public boolean isCodeValid(int iCode) {
+        if (counter < SIZE) {
+            return false;
+        }
+        int iIndex = counter % SIZE;
+        long lStartTime = cListArray[iIndex].getTime();
+        long lCurrentTime = System.currentTimeMillis() / 1000L;
+        long lDelta = lCurrentTime - lStartTime;
+        if (lDelta > 2) { return false; }
+        int iEquals = 0;
+        for (int i = 0; i < SIZE; i ++) {
+            if (iCode == cListArray[i].getCode()) { iEquals ++; }
+        }
+        return (iEquals >= (SIZE - 1));
+    }
+    public void print() {
+        for (int i = 0; i < SIZE; i ++) {
+            System.out.print(i);
+            System.out.println(cListArray[i]);
+        }
+    }
+    public int getSize(){
+        return cListArray.length;
+    }
+    //public void incCounter() {
+    //  counter ++;
+    //}
+    public int getCounter() {
+      return counter;
+    }
+}
